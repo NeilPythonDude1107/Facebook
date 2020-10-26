5629958abb4afa6fa2d8a2c227bb9308496e86c1
NeilPythonDude1107 <68854595+NeilPythonDude1107@users.noreply.github.com>
2020-10-26 13:36:11 +0530

    Update firebase.js

--- src/components/config/firebase.js
+++ src/components/config/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyA3E_vDVDR4PEb-osmkO4CX_Ml8lzwOw-8",
-    authDomain: "facebook-galaxy9.firebaseapp.com",
-    databaseURL: "https://facebook-galaxy9.firebaseio.com",
-    projectId: "facebook-galaxy9",
-    storageBucket: "facebook-galaxy9.appspot.com",
-    messagingSenderId: "688565635171",
-    appId: "1:688565635171:web:628172e388eba2469c1056",
-    measurementId: "G-K1EQCCNEDD"
+    //Your Firebase details
 })
 
 const db = firebaseApp.firestore()
